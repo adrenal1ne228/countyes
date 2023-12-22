c7a7999d914e4471d854318c9f45fd05c111aa29
adrenal1ne228 <149685357+adrenal1ne228@users.noreply.github.com>
2023-12-23 02:07:46 +0400

    Update index.js

--- index.js
+++ index.js
@@ -15,54 +15,82 @@ function start(){
         let result=0;
         let quality;
 
-        if(l1=='YEREVAN'){
+        if(l1=='Yerevan'){
             result+=1;
             getElementById('left_1').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_1').style.backgroundColor = 'red';
         }
-        if(l2=='WASHINGTON'){
+        if(l2=='Washington'){
             result+=1;
             getElementById('left_2').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_2').style.backgroundColor = 'red';
         }
-        if(l3=='PEKIN'){
+        if(l3=='Pekin'){
             result+=1;
             getElementById('left_3').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_3').style.backgroundColor = 'red';
         }
-        if(l4=='LONDON'){
+        if(l4=='London'){
             result+=1;
             getElementById('left_4').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_4').style.backgroundColor = 'red';
         }
-        if(l5=='MADRID'){
+        if(l5=='Madrid'){
             result+=1;
             getElementById('left_5').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_5').style.backgroundColor = 'red';
         }
-        if(l6=='MOSCOW'){
+        if(l6=='Moscow'){
             result+=1;
             getElementById('left_6').style.backgroundColor = 'green';
         }
         else{
             getElementById('left_6').style.backgroundColor = 'red';
         }
-        if(r1=='BERLIN'){
+        if(r1=='Berlin'){
             result+=1;
             getElementById('right_1').style.backgroundColor = 'green';
         }
         else{
             getElementById('right_1').style.backgroundColor = 'red';
+        }
+        if(r2=='Mexico'){
+            result+=1;
+            getElementById('right_2').style.backgroundColor = 'green';
+        }
+        else{
+            getElementById('right_2').style.backgroundColor = 'red';
+        }
+        if(r3=='Ottava'){
+            result+=1;
+            getElementById('right_3').style.backgroundColor = 'green';
+        }
+        else{
+            getElementById('right_3').style.backgroundColor = 'red';
+        }
+        if(r4=='Paris'){
+            result+=1;
+            getElementById('right_4').style.backgroundColor = 'green';
+        }
+        else{
+            getElementById('right_4').style.backgroundColor = 'red';
+        }
+        if(r5=='Kyiv'){
+            result+=1;
+            getElementById('right_5').style.backgroundColor = 'green';
+        }
+        else{
+            getElementById('right_5').style.backgroundColor = 'red';
         }
         if(result<=3){
             quality='Anbavarar';
@@ -74,5 +102,8 @@ function start(){
             getElementById('answer').style.backgroundColor = 'green';
             getElementById('quality').style.backgroundColor = 'green';
         }
+
+        getElementById('answer').innerHTML = result;
+        getElementById('quality').innerHTML = quality;
     }
 }
